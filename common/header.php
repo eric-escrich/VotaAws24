fca87adcf23a03572dbed7b8ba3ed13019befdf3
Eric Escrich <eric@escrich.net>
2024-06-15 23:23:32 +0200

    Implemented responsive header with dropdown navigation

--- common/header.php
+++ common/header.php
@@ -1,61 +1,83 @@
+<?php
+try {
+    session_start();
+    require 'data/dbAccess.php';
+
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $pw);
+
+    $saludo = "Bienvenido!";
+    $dashboard = false;
+    $createPoll = false;
+    $listPolls = false;
+    $myVotes = false;
+    $validAcount = false;
+    $logout = false;
+    $login = true;
+    $register = true;
+    $userName = false;
+
+    if (isset($_SESSION["usuario"])) {
+        $query = $pdo->prepare("SELECT * FROM User WHERE user_id = :id");
+        $query->execute([':id' => $_SESSION["usuario"]]);
+        $userRow = $query->fetch();
+
+        if ($userRow) {
+            if (isset($userRow['customer_name'])) {
+                $_SESSION["nombre"] = $userRow['customer_name'];
+                $saludo = "Bienvenido " . $userRow['customer_name'] . "!";
+                $userName = true;
+                $logout = true;
+                $login = false;
+                $register = false;
+
+                if ($userRow['is_mail_valid'] && $userRow['conditions_accepted']) {
+                    $dashboard = true;
+                    $createPoll = true;
+                    $listPolls = true;
+                    $myVotes = true;
+                    $validAcount = true;
+                }
+
+            }
+        }
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
+
 <nav class="navbar">
     <ul>
-        <a href="/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 19h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v5.5" />
-                <path d="M19 22v-6" />
-                <path d="M22 19l-3 -3l-3 3" />
-                <path d="M3 7l9 6l9 -6" />
-            </svg></a>
-        <li><a href="index.php">Inicio</a></li>
-        <?php
-        try {
-            session_start();
-            require 'data/dbAccess.php';
-
-            $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $pw);
-
-            $saludo = "Bienvenido!";
-
-            if (isset($_SESSION["usuario"])) {
-                $query = $pdo->prepare("SELECT * FROM User WHERE user_id = :id");
-                $query->execute([':id' => $_SESSION["usuario"]]);
-                $row = $query->fetch();
-
-                if ($row) {
-                    if (isset($row['customer_name'])) {
-                        $_SESSION["nombre"] = $row['customer_name'];
-                        $saludo = "Bienvenido " . $row['customer_name'] . "!";
-                    }
-                    if (isset($row['customer_name'])) {
-                        if ($row['is_mail_valid'] && $row['conditions_accepted']) {
-                            echo "<li><a href='dashboard.php'>DashBoard</a></li>";
-                            echo '<li><a href="create_poll.php">Crear Encuesta</a></li>';
-                            echo '<li><a href="list_polls.php">Mis encuestas</a></li>';
-                            echo '<li><a href="myVotes.php">Mis votos</a></li>';
-                        } else {
-                            echo '<li><a href="mail_verification.php">Acaba de registrarte</a></li>';
-                        }
-                        echo '<li><a href="logout.php">Cerrar Sesión</a></li>';
-                    } else {
-                        echo '<li><a href="login.php">Iniciar Sesión</a></li>';
-                        echo '<li><a href="register.php">Registrarse</a></li>';
-                    }
-                }
-            } else {
-                echo '<li><a href="login.php">Iniciar Sesión</a></li>';
-                echo '<li><a href="register.php">Registrarse</a></li>';
-            }
-
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-        ?>
-    </ul>
-    <ul>
         <li>
+            <a href="/index.php">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 19h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v5.5" />
+                    <path d="M19 22v-6" />
+                    <path d="M22 19l-3 -3l-3 3" />
+                    <path d="M3 7l9 6l9 -6" />
+                </svg>
+            </a>
+        </li>
+        <li class="spacer"></li>
+        <li class="saludo">
             <?php echo $saludo ?>
         </li>
     </ul>
 </nav>
+
+<script>
+    const menuItems = {
+        dashboard: <?php echo json_encode($dashboard); ?>,
+        createPoll: <?php echo json_encode($createPoll); ?>,
+        listPolls: <?php echo json_encode($listPolls); ?>,
+        myVotes: <?php echo json_encode($myVotes); ?>,
+        validAcount: <?php echo json_encode($validAcount); ?>,
+        logout: <?php echo json_encode($logout); ?>,
+        login: <?php echo json_encode($login); ?>,
+        register: <?php echo json_encode($register); ?>
+    };
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="componentes/handleNav.js"></script>
---
+++ componentes/handleNav.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    // Función para renderizar los elementos del menú
+    function renderMenuItems() {
+        let itemsHTML = '';
+        if (menuItems.dashboard) itemsHTML += '<li class="navLink"><a href="dashboard.php">Dashboard</a></li>';
+        if (menuItems.createPoll) itemsHTML += '<li class="navLink"><a href="create_poll.php">Crear Encuesta</a></li>';
+        if (menuItems.listPolls) itemsHTML += '<li class="navLink"><a href="list_polls.php">Mis encuestas</a></li>';
+        if (menuItems.myVotes) itemsHTML += '<li class="navLink"><a href="myVotes.php">Mis votos</a></li>';
+        if (menuItems.logout && !menuItems.validAcount) itemsHTML += '<li class="navLink"><a href="mail_verification.php">Acaba de registrarte</a></li>';
+        if (menuItems.logout) itemsHTML += '<li class="navLink"><a href="logout.php">Cerrar Sesión</a></li>';
+        if (menuItems.login) itemsHTML += '<li class="navLink"><a href="login.php">Iniciar Sesión</a></li>';
+        if (menuItems.register) itemsHTML += '<li class="navLink"><a href="register.php">Registrarse</a></li>';
+        return itemsHTML;
+    }
+
+    // Función para ajustar la barra de navegación según el tamaño de la pantalla
+    function adjustNavbar() {
+        if ($(window).width() > 1125) {
+            sidebarVisible = false;
+            $('.hamburger-menu').remove();
+            $('.sidebar').remove();
+
+            if (!$('nav.navbar li.navLink').length) {
+                let spacer = $('nav.navbar ul li.spacer');
+                spacer.before(renderMenuItems());
+            }
+
+        } else {
+            console.log('menor a 1000');
+            sidebarVisible = false;
+            $('nav.navbar li.navLink').remove();
+
+            if (!$('.hamburger-menu').length) {
+                const hamburgerMenu = $('<li>', { class: 'hamburger-menu' });
+                const svgElement = $(`
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M4 7h16" />
+                        <path d="M4 12h16" />
+                        <path d="M4 17h16" />
+                    </svg>
+                `);
+                hamburgerMenu.append(svgElement);
+                $('nav.navbar ul').prepend(hamburgerMenu);
+            }
+        }
+    }
+
+    // Ajusta la barra de navegación cuando se redimensiona la ventana
+    $(window).resize(adjustNavbar);
+
+    let sidebarVisible = false;
+    // Evento de clic para mostrar/ocultar la barra lateral
+    $('nav').on('click', '.hamburger-menu',handleSidebar);
+    $(document).on('click', '.close-icon', handleSidebar);
+    $(document).on('click', '.overlay', handleSidebar);
+    
+    function handleSidebar() {
+        console.log('click');
+        if (!sidebarVisible) {
+            const overlay = $('<div>', { class: 'overlay' });
+            $('body').append(overlay);
+            const sidebar = $('<div>', { class: 'sidebar' });
+            const closeIcon = $('\
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>\
+                    <path d="M18 6L6 18M6 6l12 12" />\
+                </svg>\
+            ');
+            const closeIconContainer = $('<div>', { class: 'close-icon' });
+            closeIconContainer.append(closeIcon);
+            sidebar.append(closeIconContainer);
+            const sidebarList = $('<ul>');
+            sidebarList.append(renderMenuItems());
+            sidebar.append(sidebarList);
+
+            
+            $('nav.navbar').after(sidebar);
+            sidebarVisible = true;
+        } else {
+            $('.sidebar').remove();
+            $('.overlay').remove();
+            sidebarVisible = false;
+        }
+    }
+});
