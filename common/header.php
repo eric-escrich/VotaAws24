a2253a8697ecc5d0c3f396673bd0a89b31af842c
Eric Escrich <eric@escrich.net>
2024-06-16 22:54:30 +0200

    Changed header to vertically expand from the side in responsive mode

--- common/header.php
+++ common/header.php
@@ -1,57 +1,61 @@
+<script src="componentes/darkmode.js"></script>
 <?php
+session_start();
+require 'data/dbAccess.php';
+
 try {
-    session_start();
-    require 'data/dbAccess.php';
-
     $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $pw);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    die();
+}
 
-    $saludo = "Bienvenido!";
-    $dashboard = false;
-    $createPoll = false;
-    $listPolls = false;
-    $myVotes = false;
-    $validAcount = false;
-    $logout = false;
-    $login = true;
-    $register = true;
-    $userName = false;
+$saludo = "Bienvenido!";
+$dashboard = false;
+$createPoll = false;
+$listPolls = false;
+$myVotes = false;
+$validAcount = false;
+$logout = false;
+$login = true;
+$register = true;
+$userName = false;
 
-    if (isset($_SESSION["usuario"])) {
-        $query = $pdo->prepare("SELECT * FROM User WHERE user_id = :id");
-        $query->execute([':id' => $_SESSION["usuario"]]);
-        $userRow = $query->fetch();
+if (isset($_SESSION["usuario"])) {
+    $query = $pdo->prepare("SELECT * FROM User WHERE user_id = :id");
+    $query->execute([':id' => $_SESSION["usuario"]]);
+    $userRow = $query->fetch(PDO::FETCH_ASSOC);
 
-        if ($userRow) {
-            if (isset($userRow['customer_name'])) {
-                $_SESSION["nombre"] = $userRow['customer_name'];
-                $saludo = "Bienvenido " . $userRow['customer_name'] . "!";
-                $userName = true;
-                $logout = true;
-                $login = false;
-                $register = false;
+    if ($userRow) {
+        if (isset($userRow['customer_name'])) {
+            $_SESSION["nombre"] = $userRow['customer_name'];
+            $saludo = "Bienvenido " . $userRow['customer_name'] . "!";
+            $userName = true;
+            $logout = true;
+            $login = false;
+            $register = false;
 
-                if ($userRow['is_mail_valid'] && $userRow['conditions_accepted']) {
-                    $dashboard = true;
-                    $createPoll = true;
-                    $listPolls = true;
-                    $myVotes = true;
-                    $validAcount = true;
-                }
-
+            if ($userRow['is_mail_valid'] && $userRow['conditions_accepted']) {
+                $dashboard = true;
+                $createPoll = true;
+                $listPolls = true;
+                $myVotes = true;
+                $validAcount = true;
             }
         }
+    } else {
+        echo "<script>console.log('Usuario no encontrado en la base de datos');</script>";
     }
-} catch (PDOException $e) {
-    echo $e->getMessage();
+} else {
+    echo "<script>console.log('No hay sesión de usuario activa');</script>";
 }
 ?>
-
 <nav class="navbar">
     <ul>
         <li>
             <a href="/index.php">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg class="icono" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M12 19h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v5.5" />
                     <path d="M19 22v-6" />
@@ -62,7 +66,12 @@ try {
         </li>
         <li class="spacer"></li>
         <li class="saludo">
-            <?php echo $saludo ?>
+            <p><?php echo $saludo; ?></p>
+        </li>
+        <li class="toggle-switch">
+            <span>Modo oscuro</span>
+            <input type="checkbox" id="darkModeSwitch">
+            <label for="darkModeSwitch"></label>
         </li>
     </ul>
 </nav>
@@ -78,6 +87,7 @@ try {
         login: <?php echo json_encode($login); ?>,
         register: <?php echo json_encode($register); ?>
     };
+    console.log(menuItems);  // Añadir depuración aquí para verificar que los valores son correctos
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="componentes/handleNav.js"></script>
